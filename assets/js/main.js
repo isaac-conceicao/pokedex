f25f25389869dd9818e324385f46666300487cae
Isaac Conceição <60546623+isaac-conceicao@users.noreply.github.com>
2023-09-18 16:29:25 -0300

    add dir assets/css assests/js. rework on project structure: rename style.css to assets/css/global.css, rename script.js to assets/js/main.js. complete removal of btnTeste feature.

---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+console.log('script importado com sucesso')
--- script.js
+++
@@ -1,5 +0,0 @@
-const btnTeste = document.getElementById("btnTeste")
-
-btnTeste.addEventListener("click",function(){
-    console.log('click no botao')
-})
