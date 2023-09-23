0bab019bbc3c79d553c19d603353c2d3af23d18f
Isaac Conceição <60546623+isaac-conceicao@users.noreply.github.com>
2023-09-23 15:24:44 -0300

    add file 'pokemon-model.js'. implement convertPokeApiDetailToPokemon() method

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,9 +2,25 @@
 
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokeApiDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeApiDetail.order
+    pokemon.name = pokeApiDetail.name
+    
+    const types = pokeApiDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+    pokemon.image = pokeApiDetail.sprites.other.dream_world.front_default
+
+    return pokemon;
+}
+
 pokeApi.getPokemonDetial = (pokemon) => {
     return fetch(pokemon.url)
             .then((response) => response.json())
+            .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0,limit = 5) => {
