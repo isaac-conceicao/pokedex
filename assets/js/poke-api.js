aca76d08766657474eac14ece807054583661c79
Isaac Conceição <60546623+isaac-conceicao@users.noreply.github.com>
2023-09-23 14:18:39 -0300

    add pokemon details from APIs subURL

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,10 +2,18 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0,limit = 10) => {
+pokeApi.getPokemonDetial = (pokemon) => {
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0,limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.log(error))
+        .then((pokemons) => pokemons.map((pokeApi.getPokemonDetial)))
+        .then((detailRequest) => Promise.all(detailRequest))
+        .then((pokemonDetails) => pokemonDetails)
 }
