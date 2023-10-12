67ffc24b0abb03717cf4a79aaeeef175c6957c5c
Isaac Conceição <60546623+isaac-conceicao@users.noreply.github.com>
2023-10-12 13:17:21 -0300

    add limit on pagination to load only 1st gen pokemon records

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
-const limit = 5;
+const maxRecords = 151
+const limit = 10;
 let offset = 0;
 
 function loadPokemonItems(offset,limit){
@@ -28,5 +29,14 @@ loadPokemonItems(offset,limit)
 
 loadMoreButton.addEventListener('click',() =>{
     offset += limit
-    loadPokemonItems(offset,limit)
+    const recordCount = offset + limit
+    if(recordCount >= maxRecords){
+        const newLimit = maxRecords-offset
+        loadPokemonItems(offset, newLimit)
+        const pagination = loadMoreButton.parentElement
+        pagination.removeChild(loadMoreButton)
+        pagination.innerHTML += "end"
+    } else {
+        loadPokemonItems(offset,limit)
+    }
 })
